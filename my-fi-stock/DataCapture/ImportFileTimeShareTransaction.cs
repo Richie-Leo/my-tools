0b802a05cb4285e7bf1b2e49d49283a1e8a5bd12
Richie-Leo <yudi@sina.com>
2016-05-05 14:14:22 +0800

    修复导入分时数据

--- my-fi-stock/DataCapture/ImportFileTimeShareTransaction.cs
+++ my-fi-stock/DataCapture/ImportFileTimeShareTransaction.cs
@@ -42,76 +42,96 @@ namespace Pandora.Invest.DataCapture
 		}
 		
 		public static void DoImportFile(Database db, string file){
-//			DateTime start = DateTime.Now;
-//			//分时交易数据的文件名格式：yyyymmdd-code.xls
-//			//从文件名中读取日期、股票代码
-//			string fileName = file.Substring(file.LastIndexOf('\\')+1); //去掉路径，取文件名
-//			fileName = fileName.Substring(0, fileName.LastIndexOf('.'));  //去掉文件名后缀
-//			string[] fields = fileName.Split('-'); //dateAndCode[0]: yyyymmdd，dateAndCode[1]: 6位股票代码
-//			if(fields.Length!=2 || fields[0].Length!=8 || fields[1].Length!=6) {
-//				Info("  文件名格式不符合规范，忽略该文件");
-//				return;
-//			}
-//			string code = fields[1];
-//			DateTime date = new DateTime(
-//				int.Parse(fields[0].Substring(0, 4)), 
-//				int.Parse(fields[0].Substring(4, 2)), 
-//				int.Parse(fields[0].Substring(6, 2)));
-//			
-//			int exists = Convert.ToInt32(db.ExecScalar(
-//				"select sto_id from bas_stock where sto_code=?code",
-//				new string[]{"code"}, new object[]{code}
-//			));
-//			if(exists<=0){
-//				Info("  股票" + code + "不存在，忽略该文件");
-//				return;
-//			}
-//			int stockId = Convert.ToInt32(db.ExecScalar(
-//				string.Format("select sto_id from bas_stock where sto_code='{0}'", code), null, null
-//			));
-//			
-//			//如果已经存在日期为date的分时数据，先删除（可能在当天交易时间内导入过不完整的分时交易明细）
-//			exists = Convert.ToInt32(db.ExecScalar(
-//				string.Format("select count(*) from sto_trade_{0} where sto_id=?id and trade_time>?start and trade_time<?end", code),
-//				new string[]{"id", "start", "end"}, new object[]{ stockId, date, date.AddDays(1) }
-//			));
-//			if(exists>0){
-//				db.ExecNonQuery(string.Format("delete from sto_trade_{0} where sto_id=?id and trade_time>?start and trade_time<?end", code),
-//				                new string[]{"id", "start", "end"}, new object[]{ stockId, date, date.AddDays(1) });
-//			}
-//
-//			using(StreamReader sr = new StreamReader(file)){
-//				BulkInsert bi = db.CreateBulkInsert(
-//					string.Format("sto_trade_{0}", code), 
-//					new string[] {"sto_id", "trade_time", "price", "qty", "match_num", "type"}, 
-//					500
-//				);
-//				string line;
-//				int lineNum = 0,  importedNum = 0;
-//				while((line = sr.ReadLine()) != null){
-//					lineNum++;
-//					//前4行为无效数据，忽略空行
-//					if(lineNum<5 || string.IsNullOrEmpty(line) || line.Trim().Length<=0) continue;
-//					//交易时间为上午09:30-11:30，下午13:00-15:00，因此有效数据行以0或1开头
-//					if(line[0]!='0' && line[0]!='1') continue;
-//					//解析每一行
-//					//0:time \t 1:price \t 2:qty \t 3:match_num \t 4:type
-//					fields = line.Split('\t');
-//					if(fields.Length<5) continue;
-//					bi.PushValues(new object[] {
-//				    	stockId, 
-//				    	DateTime.Parse(date.ToString("yyyy-MM-dd") + " " + fields[0].Trim()),
-//				    	Convert.ToDecimal(fields[1].Trim()),
-//				    	Convert.ToInt32(fields[2].Trim()),
-//				    	Convert.ToInt32(fields[3].Trim()),
-//				    	fields[4].Trim().Length<=0 ? " " : fields[4].Trim()
-//					});
-//					importedNum++;
-//				}
-//				bi.Flush();
-//				TimeSpan ts = DateTime.Now - start;
-//				Info(code + "导入完成，导入" + importedNum + "条数据，耗时" + (Convert.ToInt64(ts.TotalMilliseconds)/1000.0) + "秒");
-//			}
-		}
-	}
+            DateTime start = DateTime.Now;
+            //分时交易数据的文件名格式：yyyymmdd-code.xls
+            //从文件名中读取日期、股票代码
+            string fileName = file.Substring(file.LastIndexOf('\\') + 1); //去掉路径，取文件名
+            fileName = fileName.Substring(0, fileName.LastIndexOf('.'));  //去掉文件名后缀
+            string[] fields = fileName.Split('-'); //dateAndCode[0]: yyyymmdd，dateAndCode[1]: 6位股票代码
+            if (fields.Length != 2 || fields[0].Length != 8 || fields[1].Length != 6)
+            {
+                Info("  文件名格式不符合规范，忽略该文件");
+                return;
+            }
+            string code = fields[1];
+            DateTime date = new DateTime(
+                int.Parse(fields[0].Substring(0, 4)),
+                int.Parse(fields[0].Substring(4, 2)),
+                int.Parse(fields[0].Substring(6, 2)));
+
+            int exists = Convert.ToInt32(db.ExecScalar(
+                "select sto_id from bas_stock where sto_code=?code",
+                new string[] { "code" }, new object[] { code }
+            ));
+            if (exists <= 0)
+            {
+                Info("  股票" + code + "不存在，忽略该文件");
+                return;
+            }
+            int stockId = Convert.ToInt32(db.ExecScalar(
+                string.Format("select sto_id from bas_stock where sto_code='{0}'", code), null, null
+            ));
+
+            //如果已经存在日期为date的分时数据，先删除（可能在当天交易时间内导入过不完整的分时交易明细）
+            exists = Convert.ToInt32(db.ExecScalar(
+                string.Format("select count(*) from sto_trade_{0} where sto_id=?id and trade_time>?start and trade_time<?end", code),
+                new string[] { "id", "start", "end" }, new object[] { stockId, date, date.AddDays(1) }
+            ));
+            if (exists > 0)
+            {
+                db.ExecNonQuery(string.Format("delete from sto_trade_{0} where sto_id=?id and trade_time>?start and trade_time<?end", code),
+                                new string[] { "id", "start", "end" }, new object[] { stockId, date, date.AddDays(1) });
+            }
+
+            using (StreamReader sr = new StreamReader(file))
+            {
+                TimeshareBulkInserter<object[]> bi = new TimeshareBulkInserter<object[]>(db, 
+                    string.Format("sto_trade_{0}", code), 500
+                );
+                string line;
+                int lineNum = 0, importedNum = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lineNum++;
+                    //前4行为无效数据，忽略空行
+                    if (lineNum < 5 || string.IsNullOrEmpty(line) || line.Trim().Length <= 0) continue;
+                    //交易时间为上午09:30-11:30，下午13:00-15:00，因此有效数据行以0或1开头
+                    if (line[0] != '0' && line[0] != '1') continue;
+                    //解析每一行
+                    //0:time \t 1:price \t 2:qty \t 3:match_num \t 4:type
+                    fields = line.Split('\t');
+                    if (fields.Length < 5) continue;
+                    bi.Push(new object[] {
+                                    stockId,
+                                    DateTime.Parse(date.ToString("yyyy-MM-dd") + " " + fields[0].Trim()),
+                                    Convert.ToDecimal(fields[1].Trim()),
+                                    Convert.ToInt32(fields[2].Trim()),
+                                    Convert.ToInt32(fields[3].Trim()),
+                                    fields[4].Trim().Length<=0 ? " " : fields[4].Trim()
+                                });
+                    importedNum++;
+                }
+                bi.Flush();
+                TimeSpan ts = DateTime.Now - start;
+                Info(code + "导入完成，导入" + importedNum + "条数据，耗时" + (Convert.ToInt64(ts.TotalMilliseconds) / 1000.0) + "秒");
+            }
+        }
+    }
+
+    public class TimeshareBulkInserter<T> : BulkInserter<T>
+    {
+        public TimeshareBulkInserter(Database db, string tableName, int batchSize) : base(db, tableName, new string[] {
+                "sto_id", "trade_time", "price", "qty", "match_num", "type"
+            }, batchSize)
+        { }
+
+        public override BulkInserter<T> Push(T obj)
+        {
+            object[] e = obj as object[];
+            base.Push(new object[] {
+                    e[0], e[1], e[2], e[3], e[4], e[5] 
+                });
+            return this;
+        }
+    }
 }
